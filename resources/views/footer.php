0fa9cdec2f6145ab189a248cd345cd6c8da19ad9
Haseeb-Afandi <haseebafandi1@gmail.com>
2023-07-21 18:58:24 +0500

    Re-iniatized

---
+++ resources/views/footer.php
@@ -0,0 +1,115 @@
+<footer class="mt-auto bg-gray-900 pt-8 text-gray-100 sm:pt-14 lg:pt-16">
+    <div class="container mx-auto px-8">
+        <div class="flex flex-col gap-8 lg:flex-row lg:gap-24">
+            <div class="flex flex-col items-center sm:items-start lg:w-[30%]">
+                <h3 class="mb-2 text-xl font-bold text-white">
+                    About videvo
+                </h3>
+                <p class="mb-2 text-sm text-gray-500">
+                    Videvo offers free stock videos and motion graphics for use in any project. You may use these video
+                    clips free of charge, in both personal and commercial productions. Video clips that carry the
+                    Creative Commons 3.0 license must be attributed to the original author.
+                </p>
+                <p class="text-sm text-gray-500">
+                    If you have any suggestions for the site, or would like to make a request for free stock footage
+                    please contact us at:
+                </p>
+            </div>
+            <div class="w-full text-center lg:w-2/5 lg:text-left">
+                <h3 class="mb-2 text-xl font-bold text-white">
+                    Quick Links
+                </h3>
+                <div class="flex">
+                    <ul class="w-1/2 p-0">
+                        <li>
+                            <a class="mb-2 inline-block text-sm text-gray-500 hover:text-gray-300" href="#">Stock
+                                video</a>
+                        </li>
+                        <li>
+                            <a class="mb-2 inline-block text-sm text-gray-500 hover:text-gray-300"
+                                href="#">Footage</a>
+                        </li>
+                        <li>
+                            <a class="mb-2 inline-block text-sm text-gray-500 hover:text-gray-300"
+                                href="#">Motion graphics</a>
+                        </li>
+                        <li>
+                            <a class="mb-2 inline-block text-sm text-gray-500 hover:text-gray-300"
+                                href="#">Video templates</a>
+                        </li>
+                        <li>
+                            <a class="mb-2 inline-block text-sm text-gray-500 hover:text-gray-300"
+                                href="#">Privacy policy</a>
+                        </li>
+                        <li>
+                            <a class="mb-2 inline-block text-sm text-gray-500 hover:text-gray-300"
+                                href="#">Terms and conditions</a>
+                        </li>
+                        <li>
+                            <a class="mb-2 inline-block text-sm text-gray-500 hover:text-gray-300"
+                                href="#">API</a>
+                        </li>
+                    </ul>
+                    <ul class="w-1/2 p-0">
+                        <li>
+                            <a class="mb-2 inline-block text-sm text-gray-500 hover:text-gray-300" href="#">Browse</a>
+                        </li>
+                        <li>
+                            <a class="mb-2 inline-block text-sm text-gray-500 hover:text-gray-300" href="#">Premium</a>
+                        </li>
+                        <li>
+                            <a class="mb-2 inline-block text-sm text-gray-500 hover:text-gray-300" href="#">Affiliates</a>
+                        </li>
+                        <li>
+                            <a class="mb-2 inline-block text-sm text-gray-500 hover:text-gray-300" href="#">Blog</a>
+                        </li>
+                        <li>
+                            <a class="mb-2 inline-block text-sm text-gray-500 hover:text-gray-300" href="#">Licensing</a>
+                        </li>
+                        <li>
+                            <a class="mb-2 inline-block text-sm text-gray-500 hover:text-gray-300" href="#">Contact</a>
+                        </li>
+                        <li id="link-cookie-settings">
+                            <button id="ot-sdk-btn" class="ot-sdk-show-settings mb-2 inline-block text-sm text-gray-500 hover:text-gray-300">Cookies Settings</button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="text-center lg:w-[30%] lg:text-left">
+                <h3 class="mb-2 text-xl font-bold text-white">
+                    Social Media
+                </h3>
+                <p class="mb-4 text-sm text-gray-500">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias, possimus.
+                </p>
+
+            </div>
+        </div>
+    </div>
+    <div class="mt-8 sm:mt-16">
+        <div class="container mx-auto flex flex-col items-center justify-between border-t border-gray-800 px-8 py-6 text-center sm:flex-row">
+            <div class="flex flex-col sm:flex-row sm:items-end">
+                <img src="./images/logo-placeholder.png" class="mb-2 h-8 sm:mb-0">
+                <p class="ml-2 text-sm leading-normal text-gray-500 opacity-75">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, impedit!
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js"
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ resources/views/header_main.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Videvo_Clone</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="./css/main.css" rel="stylesheet">
+    <link href="./css/home_anime.css" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <!-- HEADER STARTS HERE  !-->
+    <header class="header-transparent fixed top-0 left-0 right-0 z-50 flex flex-col lg:flex">
+        <div
+            class="flex min-h-[3.5rem] items-center justify-between gap-2 px-4 duration-300 lg:h-16 lg:gap-0 lg:transition-all max-lg:h-full w-full">
+            <div class="flex flex-1 items-center gap-3 sm:gap-4 lg:gap-8 max-lg:!gap-y-0">
+                <a href="#" class="flex flex-1 items-center lg:flex-none order-0 sm:min-w-[90px]">
+                    <img src="./images/logo-placeholder.png" width="200px" height="50px">
+                </a>
+                <div class="sengoku flex flex-1 items-center gap-3 sm:gap-4 lg:gap-8 max-lg:!gap-y-0">
+                    <div class="order-1 max-sm:ml-0 ml-auto flex items-center lg:order-2 lg:ml-0 lg:pr-4">
+                        <div class="relative">
+                            <nav class="flex-row istock-six:flex">
+                                <span class="nav-flex group relative">
+                                    <a href="#"
+                                        class="flex flex-1 items-center rounded-lg p-2 font-bold text-gray-900 hover:bg-gray-150 xl:flex-none xl:rounded-none xl:text-white/90 xl:hover:bg-transparent xl:hover:text-white">
+                                        Videos
+                                    </a>
+                                    <a href="#"
+                                        class="flex flex-1 items-center rounded-lg p-2 font-bold text-gray-900 hover:bg-gray-150 xl:flex-none xl:rounded-none xl:text-white/90 xl:hover:bg-transparent xl:hover:text-white">
+                                        templates
+                                    </a>
+                                    <a href="#"
+                                        class="flex flex-1 items-center rounded-lg p-2 font-bold text-gray-900 hover:bg-gray-150 xl:flex-none xl:rounded-none xl:text-white/90 xl:hover:bg-transparent xl:hover:text-white">
+                                        Audio
+                                    </a>
+                                    <a href="#"
+                                        class="flex flex-1 items-center rounded-lg p-2 font-bold text-gray-900 hover:bg-gray-150 xl:flex-none xl:rounded-none xl:text-white/90 xl:hover:bg-transparent xl:hover:text-white">
+                                        Collections
+                                    </a>
+                                    <a href="#"
+                                        class="flex flex-1 items-center rounded-lg p-2 font-bold text-gray-900 hover:bg-gray-150 xl:flex-none xl:rounded-none xl:text-white/90 xl:hover:bg-transparent xl:hover:text-white">
+                                        Help
+                                    </a>
+                                </span>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="flex items-center gap-2 order-2">
+                        <a href="#"
+                            class="hidden rounded-lg border-2 border-white/50 py-1 px-3 font-bold text-white hover:border-white sm:block">
+                            Join Videvo
+                        </a>
+                        <a class="rounded-lg py-1 px-3 font-bold text-white/75 hover:text-white">Log In</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="header_container">
+        <div class="header_anime_background">
+            <div class="anime_background_inner"></div>
+        </div>
+        <div class="jidai">
+            <div class="z-1 container relative mx-auto flex flex-col items-center justify-center px-8">
+                <h1 class="text-shadow mb-4 text-center text-4xl font-bold text-white lg:text-5xl 2xl:text-6xl">
+                    Free Stock Video Footage
+                </h1>
+                <p class="text-shadow mb-6 text-center text-lg font-bold leading-tight text-white lg:leading-normal">
+                    1 million+
+                    <a href="#" class="underline hover:no-underline">stock footage clips</a>
+                    ,
+                    <a href="#" class="underline hover:no-underline">Motion Graphics clips</a>
+                    ,
+                    <a href="#" class="underline hover:no-underline">Premier Pro Templates</a>
+                    ,
+                    <a href="#" class="underline hover:no-underline">After Effect Templates</a>
+                    ,
+                    <a href="#" class="underline hover:no-underline">Royalty free music</a>
+                    &
+                    <a href="#" class="underline hover:no-underline">Sound effects</a>
+                    all at your fingertips.
+                </p>
+                <div class="w-full max-w-2xl">
+                    <form class="bg-white h-14 relative flex flex-1 rounded max-w-full">
+                        <button type="submit"></button>
+                        <div class="flex justify-center"></div>
+                        <input placeholder="Search..." class="h-full min-w-0 flex-1 appearance-none rounded-tr rounded-br bg-transparent pl-4 pr-10 text-gray-800 placeholder-gray-600 hover:bg-white focus:bg-white focus:outline-none">
+                        <button type="submit" class="w-14 h-14 absolute top-0 right-0 flex items-center justify-center rounded text-gray-900"></button>
+                    </form>
+                </div>
+                <p class="mt-4 hidden gap-8 text-white sm:flex">
+                    <a href="#" class="text-white underline hover:no-underline">
+                        Videos
+                    </a>
+                    <a href="#" class="text-white underline hover:no-underline">
+                        Popular Videos
+                    </a>
+                    <a href="#" class="text-white underline hover:no-underline">
+                        Newest Videos
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
